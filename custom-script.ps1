e951c7590a6daf6f737ea2828ecbb85ce3c5bcd2
Manuel Torres <mtm41@alu.ua.es>
2022-01-07 15:36:58 +0100

    Update custom-script.ps1

--- custom-script.ps1
+++ custom-script.ps1
@@ -1,8 +1,7 @@
-Start-Sleep -Seconds 60
-$response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"}
-echo "$response" >> log.txt 
+$response = (Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"})
+echo $response
+echo $response > log.txt
 $key = $response.Content | ConvertFrom-Json
-echo "$key" >> log.txt
 $KeyVaultToken = $key.access_token
 
 $pat = (Invoke-RestMethod -Uri "https://vmss-devops-agents-kv.vault.azure.net/secrets/vmss-devops-agents-pat?api-version=2016-10-01" -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}).value
@@ -10,7 +9,7 @@ $acr = (Invoke-RestMethod -Uri "https://vmss-devops-agents-kv.vault.azure.net/se
 $agentPool = (Invoke-RestMethod -Uri "https://vmss-devops-agents-kv.vault.azure.net/secrets/vmss-devops-agents-agent-pool?api-version=2016-10-01" -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}).value
 $identity = (Invoke-RestMethod -Uri "https://vmss-devops-agents-kv.vault.azure.net/secrets/vmss-devops-agents-identityID?api-version=2016-10-01" -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}).value
 $org = (Invoke-RestMethod -Uri "https://vmss-devops-agents-kv.vault.azure.net/secrets/vmss-devops-agents-org?api-version=2016-10-01" -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}).value
-echo "$pat" >> log.txt
+
 az login --identity --username $identity
 
 docker run -e AZP_URL="$org" -e AZP_TOKEN="$path" -e AZP_AGENT_NAME="$(hostname)" -e AZP_POOL="$agentPool" -d "$acr/azdevopsagent:latest"
